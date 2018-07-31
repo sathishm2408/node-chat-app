db706d80bdd3ea06eba4e1f53324df5e0b540ed4
sathish <sathish.24081995@gmail.com>
2018-08-01 01:26:18 +0530

    add form for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -4,25 +4,40 @@ var socket=io();
 socket.on('connect',function(){
 console.log('connected to server');
 
-/*
-socket.emit('createMessage',{
+/*socket.emit('createMessage',{
 	from:"sachu@gmail.com",
 	text:"nope"
+},function(data){
+	console.log('Got it ',data);
 });
 */
-
 });
 
 socket.on('disconnect',function(){
 console.log('Disconnected from server');
 });
 
-
-/*socket.on('newEmail',function(email){
-	console.log('newEmail sent from server with ',email);
-});
-*/
-
 socket.on('newMessage',function(message){
+	console.log('newMessage',message);
+	var li=jQuery('<li></li>');
+	var t=message.from+':'+message.text;
+	li.text(t);
+	
+	jQuery('#messages').append(li);
+});
+
+jQuery('#message-form').on('submit',function(e){
+	e.preventDefault();
+	
+	socket.emit('createMessage',{
+	from:"User",
+	text:jQuery('[name=message]').val()
+},function(data){
+	console.log('Got it ',data);
+});
+});
+
+/*socket.on('newMessage',function(message){
 	console.log('newMessage ',message);
 });
+*/
--- server/server.js
+++ server/server.js
@@ -57,9 +57,10 @@ io.on('connection',(socket)=>{
 		});
 		*/
 		
-	socket.on('createMessage',function(message){
+	socket.on('createMessage',function(message,callback){
 		console.log('createMessage',message);
 		io.emit('newMessage',generateMessage(message.from,message.text));
+		callback('this is from server');
 	});
 		
 		/*
