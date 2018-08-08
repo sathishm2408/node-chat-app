2175d7e286ebc22236eae76eb9325f2d01bbe689
sathish <sathish.24081995@gmail.com>
2018-08-08 23:49:19 +0530

    scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,21 @@
 
 var socket=io();
 
+function scrollToBottom(){
+	var messages=jQuery('#messages');
+	var newMessage=messages.children('li:last-child');
+	
+	var clientHeight=messages.prop('clientHeight');
+	var scrollTop=messages.prop('scrollTop');
+	var scrollHeight=messages.prop('scrollHeight');
+	var newMessageHeight=newMessage.innerHeight();
+	var lastMessageHeight=newMessage.prev().innerHeight();
+	
+	if(clientHeight+scrollTop+newMessageHeight+lastMessageHeight>=scrollHeight){
+		messages.scrollTop(scrollHeight);
+	}
+}
+
 socket.on('connect',function(){
 console.log('connected to server');
 
@@ -26,6 +41,7 @@ socket.on('newMessage',function(message){
 		createdAt:formattedTime
 	});
 	jQuery('#messages').append(html);
+	scrollToBottom();
 	
 	/*console.log('newMessage',message);
 	var li=jQuery('<li></li>');
@@ -45,6 +61,7 @@ socket.on('newLocationMessage',function(message){
 		createdAt:formattedTime
 	});
 	jQuery('#messages').append(html);
+	scrollToBottom();
 	
 	/*var li=jQuery('<li></li>');
 	var a=jQuery('<a target="_blank">My current location</a>');
